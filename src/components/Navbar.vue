c9e068db0106606b2202ce33fa1e7e3c85c5a798
THANATAT WONGABUT <thanatat.wong@mail.kmutt.ac.th>
2021-10-20 23:41:40 +0700

    update homepage

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <v-toolbar flat color="deep-orange darken-1">
+      <v-toolbar-title class="text-uppercase white--text">
+        CS@SIT
+      </v-toolbar-title>
+      <v-divider class="mx-4" vertical></v-divider>
+      <v-btn depressed color="deep-orange darken-1"
+      href="/">
+        <span class="white--text">Home</span>
+      </v-btn>
+      <v-btn depressed color="deep-orange darken-1"
+      href="/about">
+        <span class="white--text">About</span>
+      </v-btn>
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,40 @@
 <template>
-  <hello-world />
+  <!-- Div for Home Section -->
+  <div class="home">
+    <div id="div-1"></div>
+    <!-- Div for coming up rectangular -->
+    <div id="div-2" class="center">
+      <h1 class="display-3 font-weight-light">Predict your Papaya!</h1>
+      <br />
+      <div class="flex">
+        <v-icon color="black">mdi-cloud-upload</v-icon>
+        <h4 class="font-weight-light">
+          &nbsp; Upload your papaya image that you want to classify its ripeness
+        </h4>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+export default {};
 </script>
+
+<style>
+.center {
+  text-align: center;
+}
+#div-1 {
+  padding: 10px;
+  background: white;
+}
+#div-2 {
+  padding: 50px;
+  background: #f5f5f5;
+}
+.flex {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
