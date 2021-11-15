566dac18e788fffe84e6db658bb2643922974f8e
Sakkarin2000 <sakkarin2000@gmail.com>
2021-11-15 08:03:48 +0700

    Workable AI

---
+++ src/store/index.js
@@ -0,0 +1,104 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+Vue.use(Vuex)
+const serverUrl = "http://1a5e-171-99-154-224.ngrok.io"
+export default new Vuex.Store({
+  state: {
+    showUploadBox: true,
+    result: "",
+    loading: false,
+    error:"",
+    count: {},
+
+  },
+  mutations: {
+    setShowUploadBox(state,value){
+      state.showUploadBox = value
+    },
+    setResult(state, value){
+      state.result = value
+    },
+    setLoading(state,value){
+      state.loading = value
+    },
+    setError(state, value){
+      state.error = value
+    },
+    setCount(state, value){
+      state.count = value
+    }
+  },
+  getters:{
+    getShowUploadBox(state){
+      return state.showUploadBox
+    },
+    getResult(state){
+      return state.result
+    },
+    getLoading(state){
+      return state.loading
+    },
+    getError(state){
+      return state.error
+    },
+    getCount(state){
+      return state.count
+    }
+  },
+  actions: {
+    setShowUploadBox(context,value){
+      context.commit('setShowUploadBox',value)
+    },
+    async getResult(context,formData){
+      await axios
+    .post(
+      serverUrl+"/predict/image",
+      formData,
+      { headers: { "Content-Type": "multipart/form-data" } }
+    )
+    .then(function (response) {
+      console.log(response.data);
+      if(response.data.error==null){
+      context.commit('setResult', response.data)
+      context.commit('setLoading', false)
+      if(response.data.ripeness.length > 1){
+        let countripeness= {}
+        for (let index = 0; index < response.data.ripeness.length; index++) {
+          const element = response.data.ripeness[index];
+        
+          if (countripeness[element]) {
+            countripeness[element] += 1;
+          } else {
+            countripeness[element] = 1;
+          }
+        }
+        context.commit('setCount',countripeness)
+
+      }
+      }else{
+        context.commit('setError', response.data.error)
+        context.commit('setLoading', false)
+      } 
+
+    })
+    .catch(function (error) {
+      console.log("FAILURE!!");
+      console.log(error.response);
+      context.commit('setLoading', false)
+      context.commit('setError', error.response.data.detail)
+    });
+    },
+    clearResult(context){
+      context.commit('setResult',"")
+    },
+    clearError(context){
+      context.commit('setError',"")
+    },
+    setLoading(context, value){
+      context.commit('setLoading', value)
+    }
+  },
+  modules: {
+  }
+})
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <!-- Div for Home Section -->
     <div class="home">
       <div id="div-1"></div>
       <!-- Div for coming up rectangular -->
@@ -15,10 +14,9 @@
           </h4>
         </div>
       </div>
-      <div id="div-1"></div>
     </div>
-    <!-- Div for If condition -->
-    <div style="padding-left: 30%; padding-right: 30%" v-if="resultOne">
+    <div style="margin-top: 20px" ></div>
+    <div style="padding-left: 30%; padding-right: 30% " v-if="this.$store.getters.getShowUploadBox">
       <div class="size">
         <UploadImages
           @changed="handleImages"
@@ -29,21 +27,23 @@
         />
       </div>
     </div>
-    <!-- Div for Else Condition-->
-    <div v-else>
+    <!-- Loading -->
+    <div v-if="this.$store.getters.getLoading">
       <!-- Div Please Wait (v-if= upload) -->
-      <!-- <div v-else> 
-      <div id="div-1"></div>
-      <div class="flex">
-      <v-img src="../assets/Animation.gif" max-height="300" max-width="291">
-      </v-img>
+      <div>
+        <div id="div-1"></div>
+        <div class="flex">
+          <v-img src="../assets/Animation.gif" max-height="300" max-width="291">
+          </v-img>
+        </div>
+        <div id="div-1"></div>
+        <div class="center">
+          <h2 class="display-3 font-weight-light">Please Wait...</h2>
+        </div>
       </div>
-      <div id="div-1"></div>
-      <div class="center">
-        <h2 class="display-3 font-weight-light">Please Wait...</h2>
-      </div>
-    </div> -->
-      <div id="div-1"></div>
+    </div>
+    <!-- Result -->
+    <div style="margin-top: 20" v-if="this.$store.getters.getResult!='' &&this.$store.getters.getResult.ripeness.length==1 ">
       <div class="flex">
         <div class="center">
           <h5 class="display-2 font-weight-light">Result</h5>
@@ -51,93 +51,85 @@
       </div>
       <div class="row">
         <div class="column">
-          <img v-bind:src="picture" height="500" />
+          <img v-bind:src="picture" height="400" />
         </div>
         <div class="column">
           <!-- <h5 class="display-1 font-weight-light">Number of papayas</h5>
           <h2 class="font-weight-light">Ripe: 9</h2>
           <h2 class="font-weight-light">Medium: 9</h2>
           <h2 class="font-weight-light">Unripe: 9</h2> -->
-          <h2 class="font-weight-light"></h2>
+          <h2 class="font-weight-light">{{this.$store.getters.getResult.ripeness[0]}}</h2>
+          <h2 class="font-weight-light">{{this.$store.getters.getResult.confident}}</h2>
+
         </div>
-        <div class="column">
-          <img v-bind:src="picture" height="500" />
+        <div class="column" v-if="!this.$store.getters.getResult.ripeness.toString().includes('Not Papaya')">
+          <img v-bind:src="'data:image/jpeg;base64,'+this.$store.getters.getResult.img" height="400"  />
         </div>
       </div>
     </div>
+     <div style="margin-top: 20" v-if="this.$store.getters.getResult!='' &&this.$store.getters.getResult.ripeness.length>1 ">
+      <div class="flex">
+        <div class="center">
+          <h5 class="display-2 font-weight-light">Result</h5>
+        </div>
+      </div>
+      <div class="row">
+        <div class="column">
+          <img v-bind:src="picture" height="400" />
+        </div>
+        <div class="column">
+          <!-- <h5 class="display-1 font-weight-light">Number of papayas</h5>
+          <h2 class="font-weight-light">Ripe: 9</h2>
+          <h2 class="font-weight-light">Medium: 9</h2>
+          <h2 class="font-weight-light">Unripe: 9</h2> -->
+          <h2 class="font-weight-light">{{(this.$store.getters.getCount['ripe']==null)?("Ripe: "+0):("Ripe: "+this.$store.getters.getCount['ripe'])}}</h2>
+          <h2 class="font-weight-light">{{(this.$store.getters.getCount['medium']==null)?("Medium: "+0):("Medium: "+this.$store.getters.getCount['medium'])}}</h2>
+          <h2 class="font-weight-light">{{(this.$store.getters.getCount['unripe']==null)?("Unripe: "+0):("Unripe: "+this.$store.getters.getCount['unripe'])}}</h2>
+        </div>
+        <div class="column" v-if="!this.$store.getters.getResult.ripeness.toString().includes('Not Papaya')">
+          <img v-bind:src="'data:image/jpeg;base64,'+this.$store.getters.getResult.img" height="400"  />
+        </div>
+      </div>
+    </div>
+    <div style="margin-top: 20px" ></div>
+    <div style="padding-left: 30%; padding-right: 30% " v-if="this.$store.getters.getResult!=''||this.$store.getters.getError!=''">
+      <div class="size">
+        <UploadImages
+          @changed="handleImages"
+          uploadMsg="Drop your image here, or click to select image"
+          fileError="images files only accepted"
+          :max="1"
+          maxError="Max files exceed"
+        />
+      </div>
+      <h4 style="color: red">{{this.$store.getters.getError}}</h4>
+    </div>
   </div>
+  <!-- UploadBox -->
 </template>
 
 <script>
-let predictionResult;
-//get axios method
-const axios = require("axios");
-axios
-  .get("http://9f3c-2001-fb1-12-5e79-f5be-40ae-2e32-19cf.ngrok.io/")
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .then(function () {});
-//post axios method
 import UploadImages from "vue-upload-drop-images";
-let picture;
-
-async function SendPicPrediction(formData){
-  let res = await axios
-    .post(
-      "http://9f3c-2001-fb1-12-5e79-f5be-40ae-2e32-19cf.ngrok.io/predict/image",
-      formData,
-      { headers: { "Content-Type": "multipart/form-data" } }
-    )
-    .then(function (response) {
-      console.log(response);
-      predictionResult = response.data.result;
-    })
-    .catch(function (error) {
-      console.log("FAILURE!!");
-      console.log(error);
-    });
-  console.log(predictionResult);
-  // return predictionResult;
-}
-function handleImages(files) {
-  this.picture = window.URL.createObjectURL(files[0]);
-  console.log(files[0].name);
-  this.upload = false;
-  this.resultOne = false;
-  let formData = new FormData();
-  formData.append('file',files[0]);
-  SendPicPrediction(formData);
-  /*
-                  [
-                    {
-                        "name": "Screenshot from 2021-02-23 12-36-33.png",
-                        "size": 319775,
-                        "type": "image/png",
-                        "lastModified": 1614080193596
-                        ...
-                    },
-                    ...
-                    ]
-                 */
-}
 export default {
-  data() {
-    return {
-      upload: true,
-      resultOne: true,
-      picture,
-      predictionResult: this.predictionResult,
-    };
-  },
   components: { UploadImages },
   methods: {
-    handleImages
-  },
-};
+    handleImages(files) {
+      console.log('handle Image Toggle')
+      this.picture = window.URL.createObjectURL(files[0]);
+      console.log(files[0].name);
+      let formData = new FormData();
+      formData.append("file", files[0]);
+      this.$store.dispatch("clearResult")
+      this.$store.dispatch("clearError")
+      this.$store.dispatch("setShowUploadBox",false)
+      console.log(this.$store.getters.getShowUploadBox)
+      this.$store.dispatch("setLoading",true)
+      this.$store.dispatch("getResult",formData)
+      
+    },
+  }
+  // return predictionResult;
+}
 </script>
 
 <style>
